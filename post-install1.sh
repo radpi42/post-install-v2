033867f6281520d371c1f0eac56f97998600376a
RD <radpi42@gmail.com>
2021-08-20 08:17:42 -0700

    full attempt 1

--- post-install1.sh
+++ post-install1.sh
@@ -3,7 +3,7 @@
 apt update
 apt install -y open-vm-tools open-vm-tools-desktop qemu-guest-agent
 apt upgrade -y
-apt install -y tmux python3 python3-pip git ncdu caffeine guake remmina openssh-server xrdp dkms bc dnsutils net-tools build-essential gcc make perl apt-transport-https curl samba samba-common-bin cifs-utils python3 python3-pip python3-venv git pipenv idle3
+apt install -y tmux python3 python3-pip git ncdu caffeine guake remmina openssh-server xrdp dkms bc dnsutils net-tools build-essential gcc make perl apt-transport-https curl samba samba-common-bin cifs-utils python3 python3-pip python3-venv git pipenv idle3 wget
 
 # set up .bash_aliases file
 
@@ -28,5 +28,44 @@ fi
 "  | tee -a ~/.bashrc 
 
 source ~/.bash_aliases
+
+#install additional programs and features
+
+git config --global user.name "RD"
+git config --global user.email radpi42@gmail.com
+git config --global core.editor nano
+
+
+#brostrend wifi
+sh -c 'wget deb.trendtechcn.com/install -O /tmp/install && sh /tmp/install'
+
+#additional programs
+sudo apt install -y thonny 
+
+
+#balena etcher
+curl -1sLf \
+   'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' \
+   | sudo -E bash
+
+sudo apt-get update
+sudo apt-get install balena-etcher-electron
+
+#termius
+
+curl https://www.termius.com/download/linux/Termius.deb | apt install -y
+
+#vnc viewer
+
+curl https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.21.406-Linux-x64.deb | apt install -y
+
+
+#vs-code
+
+curl https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 | apt install -y
+
+
+#brave browser
+apt install -y apt-transport-https curl && sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list && sudo apt update && sudo apt install -y brave-browser
 
 
